c3dbb3f2ddfffba44f8e5e0aafc04ef3402bdf9f
denis <mr.ivanov667@yandex.ru>
2021-03-14 14:42:19 +0500

    Updates

---
+++ precache-manifest.2fb736cf21d8e7367866751fe59383a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babd31d9be3aae4233f92e6ec3ff9b51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d66449c4e152b8defbf9",
+    "url": "/static/css/3.9df70949.chunk.css"
+  },
+  {
+    "revision": "be2d51e0d2db94b7a1d5",
+    "url": "/static/css/main.e0f2d333.chunk.css"
+  },
+  {
+    "revision": "f13bb06a1bba4115f78b",
+    "url": "/static/js/2.2183d07b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2183d07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d66449c4e152b8defbf9",
+    "url": "/static/js/3.71715ab8.chunk.js"
+  },
+  {
+    "revision": "be2d51e0d2db94b7a1d5",
+    "url": "/static/js/main.8ec3136c.chunk.js"
+  },
+  {
+    "revision": "27a498e62a90a337d46c",
+    "url": "/static/js/runtime-main.ef402410.js"
+  },
+  {
+    "revision": "14bea6c8b042c54130ca5e3710da642c",
+    "url": "/static/media/Bean Eater-0.8s-191px.14bea6c8.svg"
+  },
+  {
+    "revision": "2805b200c0202924fb99d003fe769cc8",
+    "url": "/static/media/user.2805b200.jpg"
+  }
+]);
